1e5387d2b73b807f8dbdbb31364c944ee284b3b7
shoonia <alexanderz@wix.com>
2021-06-20 17:40:24 +0300

    Init

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import commonjs from '@rollup/plugin-commonjs';
+import nodeResolve from '@rollup/plugin-node-resolve';
+import { getBabelOutputPlugin } from '@rollup/plugin-babel';
+
+const babelPlugin = getBabelOutputPlugin({
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        loose: true,
+        useBuiltIns: 'entry',
+        targets: 'defaults',
+      },
+    ],
+  ],
+});
+
+export default {
+  input: './src/index.js',
+  output: [
+    {
+      file: './dist/esm.js',
+      format: 'esm',
+    },
+    {
+      file: './dist/es5.esm.js',
+      format: 'esm',
+      plugins: [
+        babelPlugin,
+      ],
+    },
+    {
+      file: './dist/es5.cjs.js',
+      format: 'cjs',
+      plugins: [
+        babelPlugin,
+      ],
+    },
+  ],
+  plugins: [
+    commonjs(),
+    nodeResolve(),
+  ],
+};
